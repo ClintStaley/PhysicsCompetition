318f8bbe18a2c6318d1710a25356dd47b7b658ef
Clinton Staley <clintstaley@gmail.com>
2017-08-01 12:03:47 -0500

    And the rest of the initial commit

---
+++ Routes/Account/Prss.js
@@ -0,0 +1,143 @@
+var Express = require('express');
+var Tags = require('../Validator.js').Tags;
+var router = Express.Router({caseSensitive: true});
+var async = require('async');
+
+router.baseURL = '/Prss';
+
+router.get('/', function(req, res) {
+   var ssn = req.session;
+   var clause = '';
+   var fillers = [];
+   var email = req.query.email;
+   var bugs = req.bugs;
+
+   if (email) {
+      clause = " where email like ?";
+      fillers.push(email + (bugs.noEmailPfx ? '' : '%'));
+   }
+   if (!ssn.isAdmin() && !bugs.allGetsAllowed) {
+      clause += (clause ? ' and' : ' where') + ' id = ?';
+      fillers.push(ssn.id);
+   }
+   req.cnn.chkQry('select id, email from Person' + clause, fillers,
+   function(err, prsArr) {
+      res.json(prsArr);
+      req.cnn.release();
+   });
+});
+
+router.post('/', function(req, res) {
+   var vld = req.validator;  // Shorthands
+   var body = req.body;
+   var admin = req.session && req.session.isAdmin();
+   var cnn = req.cnn;
+
+   if (admin && !body.password)
+      body.password = "*";                       // Blocking password
+   body.whenRegistered = new Date();
+
+   async.waterfall([
+   function(cb) { // Check properties and search for Email duplicates
+      if (vld.hasFields(body, ["email", "lastName", "password", "role"], cb) &&
+       vld.chain(body.role === 0 || admin, Tags.noPermission)
+       .chain(body.termsAccepted || admin, Tags.noTerms)
+       .check(body.role === 0 || body.role === 1, Tags.badValue, ["role"], cb)) {
+         cnn.chkQry('select * from Person where email = ?', body.email, cb);
+      }
+   },
+   function(existingPrss, fields, cb) {  // If no duplicates, insert new Person
+      if (vld.check(!existingPrss.length, Tags.dupEmail, null, cb)) {
+         body.termsAccepted = body.termsAccepted ? new Date() : null;
+         cnn.chkQry('insert into Person set ?', body, cb);
+      }
+   },
+   function(result, fields, cb) { // Return location of inserted Person
+      res.location(router.baseURL + '/' + result.insertId).end();
+      cb();
+   }],
+   function() {
+      cnn.release();
+   });
+});
+
+router.get('/:id', function(req, res) {
+   var vld = req.validator;
+   var prs;
+
+   if (vld.checkPrsOK(req.params.id)) {
+      req.cnn.query('select * from Person where id = ?', [req.params.id],
+      function(err, prsArr) {
+         if (vld.check(prsArr.length, Tags.notFound)) {
+             prs = prsArr[0];
+             prs.termsAccepted = prs.termsAccepted
+              && prs.termsAccepted.getTime();
+             prs.whenRegistered = prs.whenRegistered
+              && prs.whenRegistered.getTime();
+             if (!req.bugs.showPwd)
+                delete prs.password;
+             res.json(prsArr);
+         }
+         req.cnn.release();
+      });
+   }
+   else {
+      req.cnn.release();
+   }
+});
+
+router.put('/:id', function(req, res) {
+   var vld = req.validator;
+   var body = req.body;
+   var admin = req.session.isAdmin();
+   var cnn = req.cnn;
+
+   async.waterfall([
+   function(cb) {
+      if (
+        (req.bugs.badPrsPutOK || vld.checkPrsOK(req.params.id, cb)) &&
+        vld.hasOnlyFields(body,
+        ["firstName", "lastName", "password", "oldPassword", "role"])
+       .chain(!("password" in body) || body.password, Tags.badValue, ["password"])
+       .chain(!body.role || admin && body.role === 1, Tags.badValue, ["role"])
+       .check(!body.password || body.oldPassword || admin, Tags.noOldPwd,
+        null, cb))
+         cnn.chkQry("select * from Person where id = ? ", req.params.id, cb);
+   },
+   function(qRes, fields, cb) {
+      if (vld.check(admin || !body.password ||
+       qRes[0].password === body.oldPassword, Tags.oldPwdMismatch, null, cb)) {
+         delete req.body.oldPassword;
+         cnn.chkQry("update Person set ? where id = ?",
+          [req.body, req.params.id], cb);
+      }
+   },
+   function(updRes, fields, cb) {
+      res.status(200).end();
+      cb();
+   }],
+   function() {
+      cnn.release();
+   });
+});
+
+router.delete('/:id', function(req, res) {
+   var vld = req.validator;
+   var bugs = req.bugs;
+
+   if (vld.checkAdmin())
+      req.cnn.query('DELETE from Person where id = ?',
+       [bugs.prsDelFails ? req.body.id : req.params.id],
+      function (err, result) {
+         if (bugs.prsDel200 ||
+          !err && vld.check(result.affectedRows, Tags.notFound))
+            res.status(200).end();
+         req.cnn.release();
+      });
+   else {
+      req.cnn.release();
+   }
+});
+
+
+module.exports = router;
---
+++ Routes/Account/Ssns.js
@@ -0,0 +1,60 @@
+var Express = require('express');
+var Tags = require('../Validator.js').Tags;
+var ssnUtil = require('../Session.js');
+var router = Express.Router({caseSensitive: true});
+
+router.baseURL = '/Ssns';
+
+router.get('/', function(req, res) {
+   var body = [], ssn;
+
+   if (req.validator.checkAdmin()) {
+      for (var cookie in ssnUtil.sessions) {
+         ssn = ssnUtil.sessions[cookie];
+         body.push({cookie: cookie, prsId: ssn.id, loginTime: ssn.loginTime});
+      }
+      res.status(200).json(body);
+   }
+   req.cnn.release();
+});
+
+router.post('/', function(req, res) {
+   var cookie;
+   var cnn = req.cnn;
+
+   cnn.chkQry('select * from Person where email = ?', [req.body.email],
+   function(err, result) {
+      if (req.validator.check(result.length && result[0].password ===
+       req.body.password, Tags.badLogin)) {
+         cookie = ssnUtil.makeSession(result[0], res);
+         res.location(router.baseURL + '/' + cookie).status(200).end();
+      }
+      cnn.release();
+   });
+});
+
+router.delete('/:cookie', function(req, res) {
+   var bugs = req.bugs;
+   var ssnExists;
+
+   if (req.validator.check(req.params.cookie === req.cookies[ssnUtil.cookieName]
+    || !bugs.noAdmSsnDel && req.session.isAdmin(), Tags.noPermission)) {
+      ssnExists = ssnUtil.deleteSession(
+       bugs.noSsnDel ? req.query.cookie : req.params.cookie);
+      res.status(ssnExists ? 200 : 400).end();
+   }
+   req.cnn.release();
+});
+
+router.get('/:cookie', function(req, res) {
+   var cookie = req.badCookieGet ? undefined : req.params.cookie;
+   var vld = req.validator;
+   var ssn = ssnUtil.sessions[cookie];
+
+   if (vld.check(ssn, Tags.notFound) && vld.checkPrsOK(ssn.id)) {
+      res.status(200).json({cookie: cookie, prsId: ssn.id, loginTime: ssn.loginTime});
+   }
+   req.cnn.release();
+});
+
+module.exports = router;
