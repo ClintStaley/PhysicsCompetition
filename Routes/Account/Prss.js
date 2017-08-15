151305e8d158fb81bf96d72517573363edb732de
Clinton Staley <clintstaley@gmail.com>
2017-08-15 10:16:35 -0500

    ome fix commments

--- Routes/Account/Prss.js
+++ Routes/Account/Prss.js
@@ -5,12 +5,12 @@ var async = require('async');
 
 router.baseURL = '/Prss';
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
    var ssn = req.session;
    var clause = '';
    var fillers = [];
    var email = req.query.email;
-   
+
    if (email) {
       clause = " where email like ?";
       fillers.push(email + ('%'));
@@ -20,117 +20,118 @@ router.get('/', function (req, res) {
       fillers.push(ssn.id);
    }
    req.cnn.chkQry('select id, email from Person' + clause, fillers,
-      function (err, prsArr) {
-         res.json(prsArr);
-         req.cnn.release();
-      });
+   function(err, prsArr) {
+      res.json(prsArr);
+      req.cnn.release();
+   });
 });
 
-router.post('/', function (req, res) {
+router.post('/', function(req, res) {
    var vld = req.validator;  // Shorthands
    var body = req.body;
    var admin = req.session && req.session.isAdmin();
    var cnn = req.cnn;
-   
+
+// CAS FIX Didn't we say to drop this?
    if (admin && !body.password)
       body.password = "*";                       // Blocking password
    body.whenRegistered = new Date();
-   
+
    async.waterfall([
-   function (cb) { // Check properties and search for Email duplicates
+   function(cb) { // Check properties and search for Email duplicates
       if (vld.hasFields(body, ["email", "lastName", "password", "role"], cb) &&
-         vld.chain(body.role === 0 || admin, Tags.noPermission)
-            .chain(body.termsAccepted || admin, Tags.noTerms)
-            .check(body.role === 0 || body.role === 1, Tags.badValue, ["role"], cb)) {
+       vld.chain(body.role === 0 || admin, Tags.noPermission)
+       .chain(body.termsAccepted || admin, Tags.noTerms)
+       .check(body.role === 0 || body.role === 1, Tags.badValue, ["role"], cb)) {
          cnn.chkQry('select * from Person where email = ?', body.email, cb);
       }
    },
-   function (existingPrss, fields, cb) {  // If no duplicates, insert new Person
+   function(existingPrss, fields, cb) {  // If no duplicates, insert new Person
       if (vld.check(!existingPrss.length, Tags.dupEmail, null, cb)) {
          body.termsAccepted = body.termsAccepted ? new Date() : null;
          cnn.chkQry('insert into Person set ?', body, cb);
       }
    },
-   function (result, fields, cb) { // Return location of inserted Person
+   function(result, fields, cb) { // Return location of inserted Person
       res.location(router.baseURL + '/' + result.insertId).end();
       cb();
    }],
-   function () {
+   function() {
       cnn.release();
    });
 });
 
-router.get('/:id', function (req, res) {
+router.get('/:id', function(req, res) {
    var vld = req.validator;
    var prs;
-   
+
    if (vld.checkPrsOK(req.params.id)) {
       req.cnn.query('select * from Person where id = ?', [req.params.id],
-         function (err, prsArr) {
-            if (vld.check(prsArr.length, Tags.notFound)) {
-               prs = prsArr[0];
-               prs.termsAccepted = prs.termsAccepted
-                  && prs.termsAccepted.getTime();
-               prs.whenRegistered = prs.whenRegistered
-                  && prs.whenRegistered.getTime();
-               
-               delete prs.password;
-               res.json(prsArr);
-            }
-            req.cnn.release();
-         });
+      function(err, prsArr) {
+         if (vld.check(prsArr.length, Tags.notFound)) {
+             prs = prsArr[0];
+             prs.termsAccepted = prs.termsAccepted
+              && prs.termsAccepted.getTime();
+             prs.whenRegistered = prs.whenRegistered
+              && prs.whenRegistered.getTime();
+
+                delete prs.password;
+             res.json(prsArr);
+         }
+         req.cnn.release();
+      });
    }
    else {
       req.cnn.release();
    }
 });
 
-router.put('/:id', function (req, res) {
+router.put('/:id', function(req, res) {
    var vld = req.validator;
    var body = req.body;
    var admin = req.session.isAdmin();
    var cnn = req.cnn;
-   
+
    async.waterfall([
-   function (cb) {
+   function(cb) {
       if (
-         (vld.checkPrsOK(req.params.id, cb)) &&
-         vld.hasOnlyFields(body,
-            ["firstName", "lastName", "password", "oldPassword", "role"])
-            .chain(!("password" in body) || body.password, Tags.badValue, ["password"])
-            .chain(!body.role || admin && body.role === 1, Tags.badValue, ["role"])
-            .check(!body.password || body.oldPassword || admin, Tags.noOldPwd,
-               null, cb))
+        (vld.checkPrsOK(req.params.id, cb)) &&
+        vld.hasOnlyFields(body,
+        ["firstName", "lastName", "password", "oldPassword", "role"])
+       .chain(!("password" in body) || body.password, Tags.badValue, ["password"])
+       .chain(!body.role || admin && body.role === 1, Tags.badValue, ["role"])
+       .check(!body.password || body.oldPassword || admin, Tags.noOldPwd,
+        null, cb))
          cnn.chkQry("select * from Person where id = ? ", req.params.id, cb);
    },
-   function (qRes, fields, cb) {
+   function(qRes, fields, cb) {
       if (vld.check(admin || !body.password ||
-            qRes[0].password === body.oldPassword, Tags.oldPwdMismatch, null, cb)) {
+       qRes[0].password === body.oldPassword, Tags.oldPwdMismatch, null, cb)) {
          delete req.body.oldPassword;
          cnn.chkQry("update Person set ? where id = ?",
-            [req.body, req.params.id], cb);
+          [req.body, req.params.id], cb);
       }
    },
-   function (updRes, fields, cb) {
+   function(updRes, fields, cb) {
       res.status(200).end();
       cb();
    }],
-   function () {
+   function() {
       cnn.release();
    });
 });
 
-router.delete('/:id', function (req, res) {
+router.delete('/:id', function(req, res) {
    var vld = req.validator;
-   
+
    if (vld.checkAdmin())
       req.cnn.query('DELETE from Person where id = ?',
-         [req.params.id],
-         function (err, result) {
-            if (!err && vld.check(result.affectedRows, Tags.notFound))
-               res.status(200).end();
-            req.cnn.release();
-         });
+       [ req.params.id],
+      function (err, result) {
+         if (!err && vld.check(result.affectedRows, Tags.notFound))
+            res.status(200).end();
+         req.cnn.release();
+      });
    else {
       req.cnn.release();
    }
--- Routes/Competition/Ctps.js
+++ Routes/Competition/Ctps.js
@@ -7,16 +7,19 @@ var async = require('async');
 router.baseURL = '/Ctps';
 
 router.get('/', function (req, res) {
-   
+
+   // CAS FIX: Pls promptly remove debug logs as early as possible.  They
+   // tend to accumulate otherwise.
    console.log("#####Recived Request###");
-   
+
+   // CAS FIX: Style says to put the "f" in function under the "r" in req
    req.cnn.chkQry('select * from CompetitionType', null, function (err, result) {
       res.json(result);
-      
+
       console.log("#####Recived CompetitionTypes###");
-      
+
       res.status(200);
-      
+
       req.cnn.release();
    });
 });
@@ -26,12 +29,18 @@ router.post('/', function (req, res) {
    var body = req.body;
    var admin = req.session && req.session.isAdmin();
    var cnn = req.cnn;
-   
-   
+   // CAS FIX One blank line at a time except in extreme separation cases.
+
    async.waterfall([
    function (cb) { // Check properties and search for title duplicates
       console.log('#####Started Posting Check####');
+
+      // CAS FIX We need to either use 1-space continuation-line indents, or
+      // six-space.  Three-space confuses continuation lines with contained
+      // body lines.
       if (vld.hasFields(body, ["title", "description"], cb) &&
+      // vld.checkAdmin.  Pls review Validator carefully and *use* its features
+      // when available.  DRY.
          vld.check(admin, Tags.noPermission, null, cb)) {
          cnn.chkQry('select * from CompetitionType where title = ?', body.title, cb);
       }
@@ -55,18 +64,18 @@ router.post('/', function (req, res) {
 
 router.get('/:id', function (req, res) {
    var vld = req.validator;
-   
-   
+
+
    console.log('#####Get #2####');
    req.cnn.query('select * from CompetitionType where id = ?', [req.params.id],
       function (err, ctpArr) {
          if (vld.check(ctpArr.length, Tags.notFound)) {
-            
+            /// CAS FIX no blank line here
             res.json(ctpArr);
          }
          req.cnn.release();
       });
-   
+   // Nor here
 });
 
 
@@ -77,7 +86,7 @@ router.put('/:id', function (req, res) {
    var body = req.body;
    var admin = req.session.isAdmin();
    var cnn = req.cnn;
-   
+
    async.waterfall([
    function (cb) {
       if (vld.hasOnlyFields(body, ["title", "description"])
@@ -98,8 +107,8 @@ router.put('/:id', function (req, res) {
       if (vld.check(titleRes.length === 1, Tags.dupTitle, null, cb))
          cnn.chkQry("update CompetitionType set ? where id = ?",
             [req.body, req.params.id], cb);
-      
-      
+
+
    },
    function (updRes, fields, cb) {
       res.status(200).end();
@@ -114,11 +123,10 @@ router.put('/:id', function (req, res) {
 
 router.delete('/:id', function (req, res) {
    var vld = req.validator;
-   
+
    if (vld.checkAdmin())
-      req.cnn.query('DELETE from CompetitionType where id = ?',
-         [req.params.id],
-         function (err, result) {
+      req.cnn.query('DELETE from CompetitionType where id = ?', [req.params.id],
+      function (err, result) {
             if (!err && vld.check(result.affectedRows, Tags.notFound))
                res.status(200).end();
             req.cnn.release();
