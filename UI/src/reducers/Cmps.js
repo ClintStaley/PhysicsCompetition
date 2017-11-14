e464872c5512fecec149a02c782f521f47b6855d
Clinton Staley <clintstaley@gmail.com>
2017-11-14 16:08:55 -0600

    Format changes

--- UI/src/reducers/Cmps.js
+++ UI/src/reducers/Cmps.js
@@ -1,10 +1,9 @@
 
 export default function Cmps(state = [], action) {
-    console.log("Cmps reducer");
-    switch(action.type) {
-        case 'UPDATE_CMPS': // Replace previous cnvs
-            return action.cmps;
-        default:
-            return state;
-    }
+   switch (action.type) {
+      case 'UPDATE_CMPS': // Replace previous cnvs
+         return action.cmps;
+      default:
+         return state;
+   }
 }
