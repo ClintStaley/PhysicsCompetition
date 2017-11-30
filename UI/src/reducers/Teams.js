7ad70e61aea48566a892ce8a607c7292d1cdd202
Clinton Staley <clintstaley@gmail.com>
2017-11-30 01:02:11 -0600

    Fix for cish error

--- UI/src/reducers/Teams.js
+++ UI/src/reducers/Teams.js
@@ -3,7 +3,7 @@ export default function Teams(state = [], action) {
       case 'UPDATE_TEAM': // Replace previous team
          return action.Teams;
       case 'UPDATE_TEAM':
-         for (int i; i < state.length; i++)
+         for (i = 0; i < state.length; i++)
             if (action.MemberData.teamId === state.Teams[i].id)
                return state;
          return state;
