767b7462b4666af29d0b560e961aa9ceadcec1fb
Clint <clintstaley@gmail.com>
2018-07-25 14:09:34 -0500

    Teams updartes

--- UI/src/reducers/teams.js
+++ UI/src/reducers/teams.js
@@ -2,25 +2,18 @@ import update from 'immutability-helper';
 
 export default function teams(state = {}, action) {
    switch (action.type) {
-      case 'GET_TEAMS': // Replace previous team
-         return action.teams;
-      case 'TOGGLE_TEAM':
-         //update allows adding new data to the object while remaining immutable
-         //will change the team.toggled state
-         return Object.assign({}, state, {
-          [action.teamId]: Object.assign({}, state[action.teamId],
-          { toggled: !state[action.teamId].toggled })
-         });
+      case 'ADD_TEAMS': // Replace previous team
+         return Object.assign({}, state, action.teams);
       case 'POPULATE_TEAM':
-         // Will add member data to the object
+         // Add membership data to a team
          return Object.assign({}, state,
           {[action.teamData.teamId]: Object.assign({},
           state[action.teamData.teamId] , {members: action.teamData.members})});
       case 'PUT_TEAM':
          var teamId = action.teamData.teamId;
          var teamData = action.teamData.newTeamData;
-         console.log(teamId);
-         console.log(teamData);
+
+         // Overwrite only actually-changed elements in the team
          return Object.assign({}, state, {[teamId]:
           Object.assign({}, state[teamId], teamData)});
       case 'DELETE_TEAM':
