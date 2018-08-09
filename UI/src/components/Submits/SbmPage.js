72344b941e7378b3a8d739083ca7fa35a7b6a167
Clint <clintstaley@gmail.com>
2018-08-09 11:48:58 -0500

    Halfway change to main.js

--- UI/src/components/Submits/SbmPage.js
+++ UI/src/components/Submits/SbmPage.js
@@ -8,10 +8,9 @@ import * as api from '../../api';
 export default class SbmPage extends Component {
    constructor(props) {
       super(props);
-console.log("SbmPage ");
-console.log(props);
+
       this.state = {
-         ctpName: "", // Name of competition type (of first and all submits)
+         ctpTitle: "", // Name of competition type (of first and all submits)
          sbms: null   // Current submission history.
       }
    }
@@ -24,8 +23,8 @@ console.log(props);
    // a single step since ctpName is essential to display of sbms.
    updateSbms = () => {
       api.getSbms(this.props.cmp.id, this.props.team.id, 1)
-      .then(sbms =>
-        api.getCtpById(sbms[0].ctpId).then(ctp => ({ctpName: ctp.name, sbms})))
+      .then(sbms => api.getCtpById(this.props.cmp.ctpId)
+         .then(ctp => ({ctpTitle: ctp.title, sbms})))
       .then((newState) => this.setState(newState));
       //.catch((errList) => dsp({type: 'SHOW_ERR', details: errList})// use props.team.id and props.cmp.id to update sbm
    }
@@ -33,14 +32,15 @@ console.log(props);
    render() {
       var cmp = this.props.cmp;
       var sbm = this.state.sbms && this.state.sbms[0];
-      var ctpName = this.state.ctpName;
+      var ctpTitle = this.state.ctpTitle;
 
       return (
          <div>
-            <h2>{ctpName}</h2>
+            <h2>{ctpTitle}</h2>
+            <h2>{cmp.title}</h2>
             <h3>{JSON.stringify(cmp.prms)}</h3>
             <h3>{sbm && JSON.stringify(sbm.content)}</h3>
-            <h3>{sbm && JSON.stringify(sbm.response)}</h3>
+            <h3>{sbm && JSON.stringify(sbm.testResult)}</h3>
          </div>
       );
    }
