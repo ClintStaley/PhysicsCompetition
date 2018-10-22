647e34dafd58918103065ef2d04922a0e448a461
Jacob Williams <jacob321williams@gmail.com>
2018-10-22 13:24:28 -0500

    Update Test Competitions

--- RESTServer/DB/TestCompetitionInjection.sql
+++ RESTServer/DB/TestCompetitionInjection.sql
@@ -3,7 +3,7 @@ use CmpDB;
 
 
 insert into Competition (title, ctpId, ownerId, description, prms)
-            VALUES ('Land Grab Test 1', 1, 1, 'make the circles as big as possible, without colliding', '{ "numCircles": 3, "goalArea": 5000.0, "obstacles": [ { "loX": 0, "hiX": 40.2, "loY": 0, "hiY": 28.7 }, { "loX": 0, "hiX": 50, "loY": 85, "hiY": 100 }, { "loX": 80, "hiX": 100, "loY": 0, "hiY": 12 } ] }"');
+            VALUES ('Land Grab Test 1', 1, 1, 'make the circles as big as possible, without colliding', '{ "numCircles": 3, "goalArea": 5000.0, "obstacles": [ { "loX": 0, "hiX": 40.2, "loY": 0, "hiY": 28.7 }, { "loX": 0, "hiX": 50, "loY": 85, "hiY": 100 }, { "loX": 80, "hiX": 100, "loY": 0, "hiY": 12 } ] }');
 
 
 
