c0701d0f8e60ea0545ca4527897238b528752707
Jacob Williams <jacob321williams@gmail.com>
2018-11-28 14:43:36 -0600

    bug fixes/testing

--- UI/src/reducers/cmps.js
+++ UI/src/reducers/cmps.js
@@ -2,9 +2,9 @@ export default function cmps(state = {}, action) {
    var cmpId = action.cmpId, teamId = action.teamId, newTeamArr, cmpData;
 
    switch (action.type) {
-      case 'GET_PRS_CMPS': 
+      case 'GET_PRS_CMPS':
       case 'GET_CMPS':     // Replace/augment existing cmps
-         return Object.keys(action.cmps).length ? 
+         return Object.keys(action.cmps).length ?
           Object.assign(action.cmps, state) : state;
       case 'PUT_CMP':      // Update only changed fields of a cmp
          cmpId = action.cmpData.cmpId;
@@ -17,17 +17,18 @@ export default function cmps(state = {}, action) {
 
          return Object.assign({}, state, {[cmpId]:
           Object.assign({}, state[cmpId], {cmpTeams: teamIds})});
+          //.map(function (x) { return parseInt(x, 10)})
       case 'ADD_TEAM':
          teamId = action.newTeamData.id;
          cmpId = action.newTeamData.cmpId;
-         newTeamArr = state[cmpId].cmpTeams.concat(teamId.toString());
+         newTeamArr = state[cmpId].cmpTeams.concat(teamId);//.toString());
 
          return Object.assign({}, state, {[cmpId]:
           Object.assign({}, state[cmpId], {cmpTeams: newTeamArr})});
        case 'DEL_TEAM':
          newTeamArr = state[cmpId].cmpTeams.filter(id => id !== teamId);
 
-         return Object.assign({}, state, {[cmpId]: 
+         return Object.assign({}, state, {[cmpId]:
           Object.assign({}, state[cmpId], {cmpTeams: newTeamArr})});
        case 'SIGN_OUT':
           return {} // Clear user state
