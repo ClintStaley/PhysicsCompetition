f12a7784014371e832c27cd3242cf9a48a0aa9db
emmalopezcode <emma@ifixit.com>
2021-03-03 16:39:51 -0600

    Bounce3D button test

--- UI/src/components/Submits/Bounce3D.js
+++ UI/src/components/Submits/Bounce3D.js
@@ -2,28 +2,37 @@ import React, { Component } from "react";
 import * as THREE from "three";
 
 export class Bounce3D extends React.Component {
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      test: 15
+    }
+  }
+
   componentDidMount() {
     const width = this.mount.clientWidth;
     const height = this.mount.clientHeight;
-    let scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
 
-    let renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor("#263238");
-    renderer.setSize(width, height);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setClearColor("#263238");
+    this.renderer.setSize(width, height);
 
-    this.mount.appendChild(renderer.domElement);
+    this.mount.appendChild(this.renderer.domElement);
 
-    let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    camera.position.z = 20;
-    camera.position.y = 5;
-    scene.add(this.camera);
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera.position.z = 20;
+    this.camera.position.y = 5;
+    this.scene.add(this.camera);
 
     this.plight = new THREE.PointLight(0xf1fff1, 1.5);
     this.alight = new THREE.AmbientLight(0x25f1ff, 1.5);
 
     this.plight.position.set(0, 50, 50);
-    scene.add(this.plight);
-    scene.add(this.alight);
+    this.scene.add(this.plight);
+    this.scene.add(this.alight);
 
     // https://redstapler.co/realistic-reflection-effect-three-js/
 
@@ -31,11 +40,10 @@ export class Bounce3D extends React.Component {
     const material = new THREE.MeshPhongMaterial({
       color: "#555",
     });
-    let cube = new THREE.Mesh(geometry, material);
-    cube.position.y = 18.5;
-    cube.position.x = -13.5;
-    scene.add(cube);
-    this.renderScene();
+    this.state.cube = new THREE.Mesh(geometry, material);
+    this.state.cube.position.y = 18.5;
+    this.state.cube.position.x = -13.5;
+    this.scene.add(this.state.cube);
 
     let firstEvent = this.props.events[0];
     console.log(firstEvent);
@@ -48,58 +56,44 @@ export class Bounce3D extends React.Component {
 
     let subjectIdx = 0;
     let eventIdx = 0;
-    let isOver = false;
+    this.state.isOver = false;
     let events = this.props.events;
     let ev = events[subjectIdx][eventIdx];
     let nextEv = events[subjectIdx][eventIdx + 1];
     let start = Date.now();
     let endOfCurrentEvent = start + nextEv.time * 1000;
+    console.log(this);
 
-    var animate = function () {
-      if (!isOver) {
-        requestAnimationFrame(animate);
-        // vx += 0.001;
-        cube.position.x += ev.velocityX / 100;
-        vy += 0.002;
-        cube.position.y -= vy;
-        renderer.render(scene, camera);
-        if (Date.now() >= endOfCurrentEvent) {
-          if (eventIdx + 1 < events[subjectIdx].length) {
-            eventIdx++;
-          } else {
-            if (subjectIdx + 1 < events.length) {
-              subjectIdx++;
-            }
-          }
-
-          ev = events[subjectIdx][eventIdx];
-          if (ev) {
-            if (eventIdx + 1 < events[subjectIdx].length) {
-              nextEv = events[subjectIdx][eventIdx + 1];
-            }
-            endOfCurrentEvent = start + ev.time * 1000;
-          } else {
-            isOver = true;
-          }
-        }
-      }
-    };
-
-    animate();
+    this.animate();
   }
 
-  renderScene = () => {
-    if (this.renderer) this.renderer.render(this.scene, this.camera);
-  };
+  animate = () => {
+    if (!this.state.isOver) {
+      requestAnimationFrame(this.animate);
+      // vx += 0.001;
+      this.renderer.render(this.scene, this.camera);
+
+      this.state.cube.position.y = this.state.test;
+    }
+
+  }
 
   render() {
+    console.log(this.state.test);
     return (
-      <div
-        style={{ height: "800px", width: "800px", marginBottom: "200px" }}
-        ref={(mount) => {
-          this.mount = mount;
-        }}
-      ></div>
+      <div>
+        <button onClick={() => {
+          const newState = {}
+          newState['test'] = this.state.test + 0.3;
+          this.setState(newState);
+        }}>hello</button>
+        <div
+          style={{ height: "800px", width: "800px", marginBottom: "200px" }}
+          ref={(mount) => {
+            this.mount = mount;
+          }}
+        ></div>
+      </div>
     );
   }
 }
