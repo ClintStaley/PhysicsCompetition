95f5c15ac6b121c92db6538103d5c3f306c80c1d
emmalopezcode <emma@ifixit.com>
2021-03-08 20:24:11 -0600

    Current Update on 3D player

--- UI/src/components/Submits/Bounce3D.js
+++ UI/src/components/Submits/Bounce3D.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import * as THREE from "three";
+import { AmbientLightProbe } from "three";
+import "./Bounce.css";
 
 export class Bounce3D extends React.Component {
-
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      test: 15
-    }
+      test: 15,
+      playing: false,
+      time: 0,
+    };
   }
 
   componentDidMount() {
+    this.model = require("./model-movie.json");
+    this.evtIdx = 0;
     const width = this.mount.clientWidth;
     const height = this.mount.clientHeight;
     this.scene = new THREE.Scene();
@@ -22,77 +26,145 @@ export class Bounce3D extends React.Component {
 
     this.mount.appendChild(this.renderer.domElement);
 
-    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 1000);
     this.camera.position.z = 20;
-    this.camera.position.y = 5;
+    this.camera.position.y = 20;
+
     this.scene.add(this.camera);
 
     this.plight = new THREE.PointLight(0xf1fff1, 1.5);
-    this.alight = new THREE.AmbientLight(0x25f1ff, 1.5);
+    this.alight = new THREE.AmbientLight(0xfff1ff, 1.5);
 
-    this.plight.position.set(0, 50, 50);
+    this.plight.position.set(0, 50, 0);
     this.scene.add(this.plight);
     this.scene.add(this.alight);
 
     // https://redstapler.co/realistic-reflection-effect-three-js/
 
-    const geometry = new THREE.SphereGeometry(2);
+    const geometry = new THREE.SphereGeometry(2, 32, 16);
     const material = new THREE.MeshPhongMaterial({
       color: "#555",
     });
-    this.state.cube = new THREE.Mesh(geometry, material);
-    this.state.cube.position.y = 18.5;
-    this.state.cube.position.x = -13.5;
-    this.scene.add(this.state.cube);
+    this.ball = new THREE.Mesh(geometry, material);
+    this.ball.position.y = 28.5;
+    this.ball.position.x = -13.5;
+    this.scene.add(this.ball);
 
-    let firstEvent = this.props.events[0];
-    console.log(firstEvent);
+    this.start;
+    this.delta;
+    this.d2 = 0;
+    this.frames = 0;
+    this.duration = this.model.events[this.model.events.length - 1].time * 100;
+    // this.animate();
 
-    let vy = firstEvent[0].velocityY;
-    let vx = firstEvent[0].velocityX;
-
-    // each event in props.events is a subject -- ie a ball
-    // each event within those subjects is a vector over time
-
-    let subjectIdx = 0;
-    let eventIdx = 0;
-    this.state.isOver = false;
-    let events = this.props.events;
-    let ev = events[subjectIdx][eventIdx];
-    let nextEv = events[subjectIdx][eventIdx + 1];
-    let start = Date.now();
-    let endOfCurrentEvent = start + nextEv.time * 1000;
-    console.log(this);
-
-    this.animate();
+    this.setup();
+    this.renderer.render(this.scene, this.camera);
   }
 
-  animate = () => {
-    if (!this.state.isOver) {
-      requestAnimationFrame(this.animate);
+  setup = () => {
+    const set = this.model.events.filter((evt) => evt.time < 0);
+
+    set.forEach((brr) => {
+      const width = brr.hiX - brr.loX;
+      const height = brr.hiY - brr.loY;
+      const geometry = new THREE.BoxGeometry(width, height, 3);
+      const material = new THREE.MeshPhongMaterial({ color: "#678" });
+      const barrier = new THREE.Mesh(geometry, material);
+      barrier.position.x = brr.loX;
+      barrier.position.y = brr.loY;
+      this.scene.add(barrier);
+      this.evtIdx++;
+    });
+  };
+
+  animate = (timestamp) => {
+    if (this.state.playing) {
+      if (!this.start) this.start = timestamp;
+
+      this.delta = timestamp - this.start;
+
+      console.log(this.delta);
+
+      if (this.delta > this.duration) return;
+
+      var evts = [];
+      while (this.model.events[this.evtIdx].time * 100 < this.delta) {
+        evts.push(this.model.events[this.evtIdx]);
+        this.evtIdx++;
+      }
+
+      console.log(evts);
+
+      for (const evt of evts) {
+        switch (evt.type) {
+          case 0:
+            this.ball.position.x = evt.x;
+            this.ball.position.y = evt.y;
+            break;
+        }
+      }
+
+      this.setState({ time: this.state.time + 1 });
       // vx += 0.001;
       this.renderer.render(this.scene, this.camera);
-
-      this.state.cube.position.y = this.state.test;
+      requestAnimationFrame(this.animate);
     }
-
-  }
+  };
 
   render() {
-    console.log(this.state.test);
     return (
       <div>
-        <button onClick={() => {
-          const newState = {}
-          newState['test'] = this.state.test + 0.3;
-          this.setState(newState);
-        }}>hello</button>
+        <button
+          onClick={() => {
+            this.setState({ playing: !this.state.playing }, () =>
+              this.animate()
+            );
+          }}
+        >
+          play
+        </button>
         <div
-          style={{ height: "800px", width: "800px", marginBottom: "200px" }}
+          style={{ height: "600px", width: "100%" }}
           ref={(mount) => {
             this.mount = mount;
           }}
         ></div>
+        <div
+          id="slider-bg"
+          onMouseOver={(event) => {
+            let vratio = (event.clientX - 80) / event.target.getBoundingClientRect().width;
+            let start = vratio * this.duration;
+            this.setState({ time: start });
+
+            var evts = [];
+            var idx = 0;
+            while (this.model.events[idx].time * 100 < start) {
+              evts.push(this.model.events[idx]);
+              idx++;
+            }
+
+
+
+
+
+          }}
+          style={{
+            height: "11px",
+            width: "calc(100%)",
+            background: "white",
+            border: "solid 1px black",
+          }}
+        >
+          <div
+            id="slider"
+            style={{
+              height: "10px",
+              width: "10%",
+              background: "rgb(189, 63, 63)",
+              position: "relative",
+            }}
+          ></div>
+        </div>
       </div>
     );
   }
