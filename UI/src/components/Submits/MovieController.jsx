5d7386b6e0c87aa2cf4a7b509ab1fcdfe483f894
emmalopezcode <emma@ifixit.com>
2021-04-06 16:56:30 -0500

    Spacing and indentation

--- UI/src/components/Submits/MovieController.jsx
+++ UI/src/components/Submits/MovieController.jsx
@@ -4,88 +4,84 @@ import { Bounce3DView } from "./Bounce3DView";
 import Slider from 'react-rangeslider';
 
 export class MovieController extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentViewIdx: 0,
-      currentOffset: 0,
-      playing: false,
-      childEventIdx: 0
-    };
-    this.duration = this.props.jsonMovie.evts[this.props.jsonMovie.evts.length - 2].time;
-    this.firstTimeStamp;
-    this.currentView = props.views[this.state.currentViewIdx];
-  }
+   constructor(props) {
+      super(props);
+      this.state = {
+         currentViewIdx: 0,
+         currentOffset: 0,
+         playing: false,
+         childEventIdx: 0
+      };
+      this.duration = this.props.jsonMovie.evts[this.props.jsonMovie.evts.length - 2].time;
+      this.currentView = props.views[this.state.currentViewIdx];
+   }
 
-  updateEventIdx(eventIdx) {
-    this.setState({ childEventIdx: eventIdx });
-  }
+   updateEventIdx(eventIdx) {
+      this.setState({ childEventIdx: eventIdx });
+   }
 
-  play = () => {
-    this.setState({ playing: true }, this.animate);
-  };
+   play = () => {
+      this.setState({ playing: true }, this.animate);
+   };
 
-  pause = () => {
-    this.setState({ playing: false });
-  };
+   pause = () => {
+      this.setState({ playing: false });
+   };
 
-  replay = () => {
-    this.firstTimeStamp = undefined;
-    this.setState({ playing: true }, this.animate);
-  };
+   replay = () => {
+      this.firstTimeStamp = undefined;
+      this.setState({ playing: true }, this.animate);
+   };
 
-  animate = (timestamp) => {
-    if (this.state.playing) {
-      if (!this.firstTimeStamp) {
-        this.firstTimeStamp = timestamp
+   animate = (timestamp) => {
+      if (this.state.playing) {
+         if (!this.firstTimeStamp) {
+            this.firstTimeStamp = timestamp
+         }
+         if (this.state.currentOffset > this.duration) {
+            this.firstTimeStamp = undefined;
+            this.pause();
+         }
+         this.setState({ currentOffset: timestamp - this.firstTimeStamp }, () => {
+            requestAnimationFrame(this.animate);
+         });
       }
-      if (this.state.currentOffset > this.duration) {
-        this.firstTimeStamp = undefined;
-        this.pause();
-      }
-      this.setState({ currentOffset: timestamp - this.firstTimeStamp }, () => {
-        requestAnimationFrame(this.animate);
-      });
-    }
-   
-  };
+   };
 
-  render() {
-
-    return (
-      <div>
-        <div>
-          <button className="bar-button" onClick={this.play}>
-            Play
-          </button>
-          <button className="bar-button" onClick={this.pause}>
-            Pause
-          </button>
-          <button className="bar-button" onClick={this.replay}>
-            Replay
-          </button>
-        </div>
-        {this.props.views.map((view, idx) => (
-          <button
-            key={idx}
-            onClick={() => this.setState({ currentViewIdx: idx })}
-          >
-            {new view().getLabel()}
-          </button>
-        ))}
-        {React.createElement(Bounce3DView, {
-          currentOffset: this.state.currentOffset,
-          movie: this.props.jsonMovie,
-        })}
-        <Slider
-          value={this.state.currentOffset}
-          max={this.duration}
-          onChange={(value) => {
-            this.setState({ currentOffset: value })
-          }}
-        />
-
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div>
+            <div>
+               <button className="bar-button" onClick={this.play}>
+                  Play
+               </button>
+               <button className="bar-button" onClick={this.pause}>
+                  Pause
+               </button>
+               <button className="bar-button" onClick={this.replay}>
+                  Replay
+               </button>
+            </div>
+            {this.props.views.map((view, idx) => (
+               <button
+                  key={idx}
+                  onClick={() => this.setState({ currentViewIdx: idx })}
+               >
+                  {new view().getLabel()}
+               </button>
+            ))}
+            {React.createElement(Bounce3DView, {
+               currentOffset: this.state.currentOffset,
+               movie: this.props.jsonMovie,
+            })}
+            <Slider
+               value={this.state.currentOffset}
+               max={this.duration}
+               onChange={(value) => {
+                  this.setState({ currentOffset: value })
+               }}
+            />
+         </div>
+      );
+   }
 }
