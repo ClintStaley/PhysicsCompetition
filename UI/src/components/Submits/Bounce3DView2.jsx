de707a29ecb1c97ef1a1b4ffc6390dfb4f7b257a
Clint Staley <clintstaley@gmail.com>
2021-06-21 14:10:15 -0700

    Adding Bounce3DView2 and ImageUtil mockups

---
+++ UI/src/components/Submits/Bounce3DView2.jsx
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import {BounceMovie} from './BounceMovie';
+import React, { Component } from "react";
+import * as THREE from "three";
+import CameraControls from "camera-controls";
+import pingAudio from '../../assets/sound/ping.mp3';
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import UIfx from 'uifx';
+
+CameraControls.install({THREE});
+
+// Display a wall of obstacles, 
+export class Bounce3DView extends React.Component {
+   static ballRadius = .1;     // Radius of ball
+
+   // Props are: {
+   //    movie: movie to display
+   //    offset: time offset from movie start in sec
+   // }
+   constructor(props) {
+      super(props);
+
+      this.ping = new UIfx(pingAudio, {volume: 0.5, throttleMs: 100});
+      
+      this.state = Bounce3DView.setOffset(
+       Bounce3DView.getInitState(props.movie), props.offset);
+   }
+
+   // Return state displaying background grid and other fixtures
+   // appropriate for |movie|
+   static getInitState(movie) {
+      let width = movie.background.width;
+      let height = movie.background.height;
+      let longDim = Math.max(width, height);
+      let scene = new THREE.Scene();
+ 
+      return {
+         trgEvts: [],    // Target creation events (each w/ptr to scene Group)
+         evtIdx: -1,     // Index within movie of last event shown in scene
+         scene,          // scene to render at this point
+         movie           // Pointer to current movie
+      }   
+   }
+
+   // Return label for button activating this view
+   static getLabel() {
+      return "3D";
+   }
+
+   static getDerivedStateFromProps(newProps, oldState) {
+      let rtn = oldState;
+
+      if (newProps.movie !== oldState.movie) // Complete reset
+         rtn = Bounce3DView.getInitState(newProps.movie);
+      return Bounce3DView.setOffset(rtn, newProps.offset);
+   }
+
+   // Advance/retract |state| so that state reflects all and only those events
+   // in |movie| with time <= |timeStamp|.  Assume existing |state| was built
+   // from |movie| so incremental change is appropriate.  Return adjusted state
+   static setOffset(state, timeStamp) {
+      let movie = state.movie;
+      let evts = movie.evts;
+      let {trgEvts, ballEvt, evtIdx, scene} = state;
+      let yTop = movie.background.height;
+      let evt;
+
+      // While the event after evtIdx exists and needs adding to 3DElms
+      while (evtIdx+1 < evts.length && evts[evtIdx+1].time <= timeStamp) {
+         evt = evts[++evtIdx];
+         if (evt.type === BounceMovie.cMakeBarrier) {
+            // Add the indicated barrier to the scene
+         }
+         else if (evt.type === BounceMovie.cMakeTarget) {
+            trgEvts[evt.id] = evt;  // Save event for redrawing if hit
+            evt.sceneElm = // Point to target scene object so it can be moved later
+            // Add indicated target to scene
+         }
+         else if (evt.type === BounceMovie.cBallPosition) {
+            // Move ball to indicated new position in scene
+         }
+         else if (evt.type === BounceMovie.cHitBarrier
+          || evt.type === BounceMovie.cHitTarget) {
+            // Move ball to indicated hit location in scene
+
+            if (evt.type === BounceMovie.cHitTarget) {
+               let trgEvt = trgEvts[evt.targetId];
+               // Find sceneElm and adjust it by popping it inward.  
+               // Play pin sound.
+            }
+         }
+         // Ball launch and ball exit require no action yet...
+      }
+
+      // Undo events to move backward in time. (Note that this and the prior
+      // while condition are mutually exclusive.) Assume that barrier and
+      // target creation occur at negative time and thus will not be "backed
+      // over"
+      while (evtIdx > 0 && timeStamp < evts[evtIdx].time) {
+         evt = evts[evtIdx--];
+         if (evt.type === BounceMovie.cBallPosition
+          || evt.type === BounceMovie.cHitBarrier 
+          || evt.type === BounceMovie.cHitTarget)
+            // Move ball back to earlier position.
+
+         if (evt.type === BounceMovie.cHitTarget) {
+            let trgEvt = trgEvts[evt.targetId];
+            // Pop indicated target back out.
+         }
+      }
+
+      // Loop from current evtIdx backward to find most recent event that draws
+      // a full ball, or null if most recent is a ballExit.
+      ballEvt = null;
+      for (let searchIdx = evtIdx; searchIdx >= 0; searchIdx--) {
+         let testEvt = evts[searchIdx];
+         if (testEvt.type === BounceMovie.cBallExit)
+            break;
+         else if (testEvt.type === BounceMovie.cBallPosition 
+          || testEvt.type === BounceMovie.cHitTarget
+          || testEvt.type === BounceMovie.cHitBarrier) {
+            ballEvt = testEvt;
+            break;
+         }
+      }
+      return {trgEvts, ballEvt, evtIdx, scene, movie};
+   }
+
+   render() {
+      // Render current scene.
+      return (
+         <div
+            style={{ height: "600px", width: "100%" }}
+            ref={(mount) => {
+               this.mount = mount;
+            }}
+         ></div>
+   }
+}
---
+++ UI/src/components/Util/ImageUtil.js
@@ -0,0 +1,66 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+// Create a MeshStandard material with the given color hex-value and the 
+// given texture(s)
+//
+// tex format.  Only root 
+// {
+//   root: "root dir"
+//   normal: "filename",
+//   displacement: {file: "filename", scale: num},
+//   roughness: "filename",
+//   ao: "filename",
+//   metal: {file: "filename", metalness: num}
+//   reps: {x: num, y: num}
+// }
+
+export function createMaterial(color, tex) {
+   let loader = new THREE.TextureLoader();
+   let path = `${window.location.origin}/textures/${tex.root}/`;
+   let reps = tex.reps;
+   
+   return new THREE.MeshStandardMaterial({
+      color,
+      normalMap: tex.normal && loadTexture(`${path}/${tex.normal}`, reps),
+      displacementMap:
+       tex.displacement && loadTexture(`${path}/${tex.displacement.file}`, reps),
+      displacementScale: tex.displacement && tex.displacement.scale,
+      roughnessMap:
+       tex.roughness && loadTexture(`${path}/${tex.roughness}, reps`),
+      aoMap: tex.ao && loadTexture(`${path}/${tex.ao}`, reps),
+      metalnessMap: tex.metal && loadTexture(`${path}/${tex.metal.file}`, reps),
+      metalness: tex.metal && tex.metal.metalness,
+   });
+}
+
+// Load GLTF asset from |url|, obtaining from it just the scene (not cameras,
+// animations, etc).  Apply the indicated transformation, and return the scene. 
+export function loadGLTFScene(url, transform) {
+   let loader = new GLTFLoader();
+   let modelUrl = `${window.location.origin}/models/${url}/scene.gltf`;
+
+   loader.load(modelUrl,
+      ({scene}) => {
+         scene.updateMatrix(transform);
+         scene.castShadow = true;
+         return scene;
+      },
+      undefined,
+      (err) => console.log(err)
+   );
+}
+
+// 
+loadTexture(path, reps) {
+   let loader = new THREE.TextureLoader();
+   
+   let texture = loader.load(path, (texture) => {
+      if (reps) {
+         texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+         texture.repeat.set(reps.x, reps.y);
+      }
+   });
+
+   return texture;
+}
