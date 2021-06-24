b52d5d4e2036d8ba684928c5304681fbf4607c06
clarkdavidson <73504343+clarkdavidson@users.noreply.github.com>
2021-06-24 10:00:50 -0500

    Added Camera to Sample3js

--- UI/src/components/Util/Sampler3JS.jsx
+++ UI/src/components/Util/Sampler3JS.jsx
@@ -1,7 +1,35 @@
+import CameraControls from "camera-controls";
 import React, { Component } from "react";
 import * as THREE from "three";
+import { Side } from "three";
 
-export class Sampler3JS extends React.Component{
+CameraControls.install({ THREE: THREE });
+
+export class Sampler3JS extends React.Component {
+   // Placeholders: light shiny metal and buff gray diffuse
+   static steelMat = new THREE.MeshStandardMaterial({metalness: 1.0});
+   static concreteMat = new THREE.MeshStandardMaterial({color: "#808080"});
+
+   static materials = [this.concreteMat,this.concreteMat,this.concreteMat,this.concreteMat,this.concreteMat,this.concreteMat]
+   static textures = [
+      {
+         root: 'steelplate1-ue',
+         normal: 'steelplate1_normal-dx.png',
+         displacement: 'steelplate1_height.png',
+         roughness: 'steelplate1_roughness.png',
+         ao: 'steelplate1_ao.png',
+         metalness: 'steelplate1_metallic.png'
+      },
+      {
+         root: 'concrete',
+         normal: 'normal.jpg',
+         displacement: 'displacement.png',
+         roughness: 'roughness.png',
+         ao: 'ao.png',
+         metalness: 'basecolor.png'
+      }
+   ]
+
    constructor(props) {
       super(props);
 
@@ -16,39 +44,55 @@ export class Sampler3JS extends React.Component{
 
       const ambLight = new THREE.AmbientLight(0x808080);
       const pntLight = new THREE.PointLight(0xFFFFFF, .8, 0, 2);
-      
-      camera.position.z = 10;
+
+      camera.position.set(0, 0, 1);
       pntLight.position.set(5, 5, 10);
-      
+
+      var cubeGeo = new THREE.BoxGeometry(60, 60, 60, this.materials);
+      var cube = new THREE.Mesh(cubeGeo, new THREE.MeshFaceMaterial());
+
       scene.add(ambLight);
       scene.add(pntLight);
-      scene.add(new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), mat));
-      
+      scene.add(cube);
+      console.log(scene)
+      console.log(camera.position)
+
+      const cameraHelper = new THREE.CameraHelper(camera);
+      scene.add(cameraHelper);
+
       return {
          scene,
          camera,
          renderer
       }
    }
-   
+
    // Do state setup dependent on this.mount, including:
    //
    // 1. Set size of renderer.
    // 2. Adjust camera aspect ratio from default initial value of 1.
    // 3. Attach the renderer dom element to the mount.
    // 4. Do a render
-   componentDidMount(){
+   componentDidMount() {
+      
       const width = this.mount.clientWidth;
       const height = this.mount.clientHeight;
-      
+
       this.state.renderer.setSize(width, height);
 
-      this.state.camera.aspect = width/height;
+      this.state.camera.aspect = width / height;
       this.state.camera.updateProjectionMatrix();
-      
-      this.mount.appendChild(this.state.renderer.domElement);
-      this.state.renderer.render(this.state.scene, this.state.camera);
 
+      this.mount.appendChild(this.state.renderer.domElement);
+      this.cameraControls = new CameraControls(this.state.camera, this.state.renderer.domElement);
+      this.cameraControls.addEventListener("control", () => {
+         console.log(this.state.camera.position)
+         if(this.cameraControls.update(1))
+            this.state.renderer.render(this.state.scene, this.state.camera);
+      })
+
+      this.cameraControls.setTarget(0,0,0);
+      this.cameraControls.update(1);
    }
 
    render() {
