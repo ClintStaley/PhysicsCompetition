ff8856273a801ed750e6c0ef28a57131c04e86ca
Clint Staley <clintstaley@gmail.com>
2021-06-24 19:26:09 -0700

    Fixes for register include

--- UI/src/components/concentrator.js
+++ UI/src/components/concentrator.js
@@ -1,6 +1,6 @@
 export {default as App} from './App';
 export {default as SignIn} from './SignIn/SignIn';
-export {default as Register} from './Register/Register';
+export {default as Register} from './Register/Register.jsx';
 export {default as CmpsPage} from './Competitions/CmpsPage';;
 export {default as TeamsPage} from './Teams/TeamsPage';
 export {default as ConfDialog} from './ConfDialog/ConfDialog';
